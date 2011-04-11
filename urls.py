77ff9b1b6c48ffcd621eea9394fa912fe0de42ca
Anton Pirker <anton@ignaz.at>
2011-04-11 22:02:01 +0200

    ~ reorganizing files

--- urls.py
+++ urls.py
@@ -1,30 +1,10 @@
 # -*- coding: utf-8 -*-
+
 from django.conf import settings
 from django.conf.urls.defaults import *
-from views import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', home, name='home'),
-    
-    url(r'^static/$', static, name='static'),
-    
-    url(r'^simple_db_query/$', simple_db_query, name='simple_db_query'),
-    url(r'^sequential_db_query/$', sequential_db_query, name='sequential_db_query'),
-    url(r'^complex_db_query/$', complex_db_query, name='complex_db_query'),
-    
-    url(r'^db_write/$', db_write, name='db_write'),
-    
-    url(r'^upload/$', upload, name='upload'),
-        
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),       
+    (r'^$', include('benchmark_app.urls')),
 )
 
 # the assets
@@ -32,4 +12,3 @@ if settings.SERVE_MEDIA:
     urlpatterns += patterns('',
         (r'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
-
